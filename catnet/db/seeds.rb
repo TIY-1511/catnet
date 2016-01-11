3271dc895cec486ea0d58894fa66ffc7e15a41e0
trainer <trainer@mac.local>
2016-01-11 20:03:18 +0000

    adding code

---
+++ catnet/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cat.create([{ name: 'Grump Cat', description: 'Grumpy Cat (born April 4, 2012), real name Tardar Sauce, is a female cat and Internet celebrity known for her grumpy facial expression.', url: 'http://cdn.cutestpaw.com/wp-content/uploads/2013/12/Most-Famous-Felines-005.jpg' }, 
+			{ name: 'Maru', description: 'Maru (まる, Japanese: circle or round; born May 24, 2007) is a male Scottish Fold. He has been amusing fans with his YouTube antics for years.', url: 'http://cdn.cutestpaw.com/wp-content/uploads/2013/12/Most-Famous-Felines-001.jpg' },
+			{ name: 'Snoopy the Cat', description: 'Snoopy the Cat (大肥猫宝儿) is one of the most popular cats in the world especially in China. She is an Exotic Short hair cat with a lovely master that takes pictures of her everyday.', url: 'http://cdn.cutestpaw.com/wp-content/uploads/2013/12/Most-Famous-Felines-010.jpg' },
+			{ name: 'Lil Bub', description: 'Lil Bub (born June 21, 2011) is an American cat and Internet celebrity known for her “perma-kitten” appearance.', url: 'http://cdn.cutestpaw.com/wp-content/uploads/2013/12/Most-Famous-Felines-013.jpg' },
+			{ name: 'Colonel Meow', description: 'Colonel Meow first found fame in 2012 and has since gained hundreds of thousands of fans (or minions, as he calls them) on social networks.', url: 'http://cdn.cutestpaw.com/wp-content/uploads/2013/12/Most-Famous-Felines-016.jpg' },
+			{ name: 'Cooper', description: 'Cooper is a 6 year old American Shorthair cat living in Seattle.', url: 'http://cdn.cutestpaw.com/wp-content/uploads/2013/12/Most-Famous-Felines-020.jpg' }])
